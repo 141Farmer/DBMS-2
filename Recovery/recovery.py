c49f21d29b919e41804456a7d21c4729937cb1d4
kamrul <bsse1418@iit.du.ac.bd>
2024-10-05 20:31:30 +0600

    fl

--- Recovery/recovery.py
+++ Recovery/recovery.py
@@ -5,8 +5,7 @@ def recover(log):
           redo=[]
           undo=[]
           ignore=[]
-          ckpt=False
-          transactions=[]
+          ckpt=False       
           for itr in reversed(log):
                     c=itr[1:-1].split(' ')
                     if 'CKPT(' in c[0]:
@@ -17,18 +16,36 @@ def recover(log):
                               redo.append(c[1])
                     elif c[0]=='START' and c[1] not in redo and c[1] not in ignore:
                               undo.append(c[1])
-
           return ignore,undo,redo
 
+def value(log,ignore,undo,redo):
+          transactionValues=[[],[]]
+          for itr in log:
+                    c=itr[1:-1].split(' ')
+                    if len(c)==4 and c[1] in transactionValues[0]:
+                              continue
+                    if 'T' in c[0] and c[0] in redo:
+                              transactionValues[0].append(c[1])
+                              transactionValues[1].append(c[3])
+                    elif 'T' in c[0] and c[0] in undo:
+                              transactionValues[0].append(c[1])
+                              transactionValues[1].append(c[2])
+                    elif 'T' in c[0] and c[0] in ignore:
+                              transactionValues[0].append(c[1])
+                              transactionValues[1].append(c[3])
+          return transactionValues
 
 def main():
           with open(Path(__file__).parent/'input.pdf') as file:
                    log=file.read()               
           log=log.splitlines()
           ignore,undo,redo=recover(log)
+          transactionValues=value(log,ignore,undo,redo)
           print('Ignore :',*ignore)
           print('Undo :',*undo)
           print('Redo :',*redo)
+          for i in range(len(transactionValues[0])):
+                    print(transactionValues[0][i],transactionValues[1][i])
 
 if __name__=='__main__':
           main()
